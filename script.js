4163b1db105a54271b854d5108808a2ac5f9c0f8
shaktisingh647 <consolelogshakti@gmail.com>
2025-10-24 09:05:01 +0530

    commit

--- script.js
+++ script.js
@@ -19,8 +19,7 @@ setIndicator("#ccc");
  
 function handleSlider() {
     inputSlider.value = passwordLength;
-    console.log("shakti singh");
-    console.log("hello world");
+    
     lengthDisplay.innerHTML=passwordLength;
     
 
